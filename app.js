975854e2235bfe19028701cb2294ef1efa83aa66
Karina Dalca <Karina@Karinas-MacBook-Air.local>
2017-04-18 15:34:19 -0700

    wiring things up

--- app.js
+++ app.js
@@ -3,20 +3,26 @@ const add = require('./adder.js');
 class ViewManager {
 
 	connectEventHandlers() {
-		// wire up even handler for form submit
+		document.getElementById('form-numbers')
+		.addEventListener(
+			'submit', 
+			this.onSubmit);
 	}
 
 	onSubmit(event) {
-		// block form from actually submitting
-		// (which would refresh the page)
+		event.preventDefault();
 
-		// grab the number values as strings
+		let num1 = document.getElementById(
+			'input-num1').value;
+		let num2 = document.getElementById(
+			'input-num2').value;
 
-		// cast the string to ints
+		num1 = parseInt(num1, 10);
+		num2 = parseInt(num2, 10);
 
-		// add the numbers
+		const sum = add(num1, num2);		
 
-		// output
+		alert(sum);
 	}
 }
 
