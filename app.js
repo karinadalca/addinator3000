6c22ee10c6346cf0667ce2e84185505dc7c1079f
Karina Dalca <Karina@Karinas-MacBook-Air.local>
2017-04-18 15:03:03 -0700

    add skeleton for view management

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 const add = require('./adder.js');
 
-console.log(add(5,6));
+class ViewManager {
+
+	connectEventHandlers() {
+		// wire up even handler for form submit
+	}
+
+	onSubmit(event) {
+		// block form from actually submitting
+		// (which would refresh the page)
+
+		// grab the number values as strings
+
+		// cast the string to ints
+
+		// add the numbers
+
+		// output
+	}
+}
+
+const viewManager = new ViewManager();
+viewManager.connectEventHandlers();
